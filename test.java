871b31c5bae5d1fff6f926c62fc0b192816cd501
Deepa-j1997 <78294795+Deepa-j1997@users.noreply.github.com>
2021-02-11 21:01:58 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,21 @@
+package com.test;
+
+import Assign6.Rectangle;
+import Assign6.Square;
+
+public class test {
+		public static void main(String[] args) {
+			
+		
+		Square square=new Square(5);
+		Rectangle rect=new Rectangle(12,8);
+		square.calcArea();
+		square.calcPeri();
+		rect.calcArea();
+		rect.calcPeri();
+		
+		}  
+	}
+
+
+
